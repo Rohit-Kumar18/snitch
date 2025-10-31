8fedfd28461049acfadc9784390c742152cce19a
Rohit <rohitkumarweb18@gmail.com>
2025-10-31 15:30:01 +0530

    header was optimised for mobile orientation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,19 @@ import Search from "./Search";
 const Header = () => {
   return (
     <div className="fixed top-0 left-0 w-full z-50">
-      <div className="h-24 border place-content-between flex pl-[10%] bg-[#FFFFFF] border-b border-gray-300 items-center">
-        <div className="border border-transparent w-[300px] h-[76px] flex justify-center items-center mx-20 cursor-pointer">
+      <div className="h-20 sm:h-24 items-center justify-between flex flex-wrap px-4 sm:px-10 bg-[#FFFFFF] border border-b border-gray-300 ">
+        <div className="w-full sm:w-auto py-2 sm:py-0 flex justify-center sm:justify-start cursor-pointer">
           <Link to={"/"}>
             <img
-              className="w-[270px] object-cover object-center"
+              className="w-48 sm:w-[270px] object-cover object-center"
               src={LOGO_URL}
             />
           </Link>
         </div>
 
-        <Search />
+        <div className="hidden sm:block flex-grow mx-4">
+          <Search />
+        </div>
 
         {/* <div className="flex  items-center mr-10">
           <ul className="flex text-xl">
@@ -37,39 +39,31 @@ const Header = () => {
           </ul>
         </div> */}
 
-        <div className="flex  items-center mr-10 w-[300px] place-content-around">
-          {/* <div className="w-12 h-14 relative  cursor-pointer">
+        <div className="w-full sm:w-auto flex justify-center sm:justify-end items-center gap-4 mt-2 sm:mt-0 relative cursor-pointer">
+          <Link to={"/"}>
+            <h1 className="uppercase sm:text-xl text-sm p-2 hover:text-blue-500 transition-transform duration-300 hover:scale-110">
+              Home
+            </h1>
+          </Link>
+          <Link to={"/men-new-arrivals"}>
+            <h1 className="uppercase text-sm sm:text-xl p-2 hover:text-blue-500 transition-transform duration-300 hover:scale-110">
+              New
+            </h1>
+          </Link>
+          <Link to={"/cart"}>
             <img
-              className="w-full h-full object-center object-contain transition-transform duration-300 hover:scale-120"
-              src={HOME_IMG}
+              className="w-6 sm:w-10 h-6 sm:h-12 object-contain transition-transform duration-200 hover:scale-110"
+              src={CART_IMG}
+              alt="Cart"
             />
-          </div> */}
-          <div className="w-18 relative p-2 cursor-pointer">
-            <h1 className="uppercase text-xl transition-transform duration-300 hover:text-blue-500 hover:scale-110">
-              <Link to={"/"}>Home</Link>
-            </h1>
-          </div>
-          <div className="w-18 relative p-2 cursor-pointer">
-            <h1 className="uppercase text-xl transition-transform duration-300 hover:text-blue-500 hover:scale-110">
-              <Link to={"/men-new-arrivals"}>New</Link>
-            </h1>
-          </div>
-          <div className="w-10 h-12 relative cursor-pointer">
-            <Link to={"/cart"}>
-              <img
-                className="w-full h-full object-center object-contain transition-transform duration-200 hover:scale-110"
-                src={CART_IMG}
-              />
-            </Link>
-          </div>
-          <div className="w-10 h-12 relative cursor-pointer">
-            <Link to={"/user-info"}>
-              <img
-                className="w-full h-full object-center object-contain transition-transform duration-250 hover:scale-110"
-                src={ADMIN_IMG}
-              />
-            </Link>
-          </div>
+          </Link>
+          <Link to={"/user-info"}>
+            <img
+              className="w-6 sm:w-10 h-6 sm:h-12 object-contain transition-transform duration-250 hover:scale-110"
+              src={ADMIN_IMG}
+              alt="User"
+            />
+          </Link>
         </div>
       </div>
     </div>
